3f85c72fb55ee851b0c8a6c1c2467bff8367fa74
VictorinaVicka <city_br@mail.ru>
2019-10-12 18:51:42 +0300

    Изменения после разговора в 18-21 11.10

--- Dream Team/App/AppDelegate.swift
+++ Dream Team/App/AppDelegate.swift
@@ -10,10 +10,11 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UINavigationBar.appearance().barTintColor = .red
+      
+        UINavigationBar.appearance().barTintColor = .blue
         
         return true
     }
